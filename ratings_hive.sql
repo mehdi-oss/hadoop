0e42dd1f7b8c7f2bc4cba0ce12327bce88f3d96c
Mehdi Medrani <59346013+mehdi-oss@users.noreply.github.com>
2022-08-30 14:11:50 +0100

    Update ratings_hive.sql

--- ratings_hive.sql
+++ ratings_hive.sql
@@ -1,36 +1,9 @@
---Drops existing database
-DROP DATABASE IF EXISTS movies CASCADE;
-
---Creates new database 
-CREATE DATABASE IF NOT EXISTS movies;
-
---Use database
-USE movies;
-
---Creates Ratings table
-CREATE TABLE Ratings
-(user_id INT,
-movie_id INT,
-rating INT,
-ts INT
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY '\t'
-LINES TERMINATED BY '\n'
-STORED AS TEXTFILE;
-
---Loads data from local data file
-LOAD DATA LOCAl INPATH '/home/maria_dev/ratings.data' 
-INTO TABLE Ratings;
-
---Get metadata of table
-DESCRIBE Ratings;
-
---Get count of table
-SELECT COUNT(*) FROM Ratings;
-
---Get the number of occurences for each ratings
-SELECT rating, count(rating) as total_occurences
-FROM Ratings
-GROUP BY rating
-ORDER BY total_occurences DESC;
+use dataweblast;
+SELECT  distinct  ref_secteurs.secteur FROM bridge_studylevel_data
+INNER JOIN ref_niveauetude ON ref_niveauetude.id = bridge_studylevel_data.study_level
+INNER JOIN bridge_sectors_data ON bridge_sectors_data.id = bridge_studylevel_data.id
+INNER JOIN ref_secteurs ON ref_secteurs.idSecteur = bridge_sectors_data.sector
+INNER JOIN bridge_exp_data ON bridge_studylevel_data.id = bridge_exp_data.id
+INNER JOIN ref_experience ON ref_experience.id = bridge_exp_data.exp_text
+and bridge_exp_data.exp_text = 3
+and bridge_studylevel_data.study_level= 11
